5de3fd134c9561b71a6c81428b614d4fecd15af1
Henry <193541T@mymail.nyp.edu.sg>
2020-11-07 22:12:28 +0800

    (7/11) Window stuff

--- AI/Source/App.cpp
+++ AI/Source/App.cpp
@@ -15,27 +15,17 @@
 GLFWwindow* m_window;
 const unsigned char FPS = 60; // FPS of this game
 const unsigned int frameTime = 1000 / FPS; // time for each frame
-int m_width, m_height;
 
-//Define an error callback
-static void error_callback(int error, const char* description)
-{
+int winWidth, winHeight;
+
+static void error_callback(int error, const char* description){
 	fputs(description, stderr);
 	_fgetchar();
 }
 
-//Define the key input callback
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods){
-	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, GL_TRUE);
-}
-
-
-void resize_callback(GLFWwindow* window, int w, int h)
-{
-	m_width = w;
-	m_height = h;
-	glViewport(0, 0, w, h);
+void resize_callback(GLFWwindow* window, int w, int h){
+	winWidth = w;
+	winHeight = h;
 }
 
 bool App::IsKeyPressed(unsigned short key)
@@ -52,11 +42,11 @@ void App::GetCursorPos(double *xpos, double *ypos)
 }
 int App::GetWindowWidth()
 {
-	return m_width;
+	return winWidth;
 }
 int App::GetWindowHeight()
 {
-	return m_height;
+	return winHeight;
 }
 
 void App::Init(){
@@ -75,24 +65,17 @@ void App::Init(){
 	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //We don't want the old OpenGL 
 
-	//Create a window and create its OpenGL context
-	m_width = 1000;
-	m_height = 600;
-	m_window = glfwCreateWindow(m_width, m_height, "Physics", NULL, NULL);
+	m_window = glfwCreateWindow(1000, 1000, "App Window", nullptr, nullptr);
+	glfwMaximizeWindow(m_window);
+	glfwGetWindowSize(m_window, &winWidth, &winHeight);
 
-	//If the window couldn't be created
-	if (!m_window)
-	{
-		fprintf( stderr, "Failed to open GLFW window.\n" );
+	if (!m_window){
+		fprintf(stderr, "Failed to open GLFW window.\n");
 		glfwTerminate();
 		exit(EXIT_FAILURE);
 	}
-
-	//This function makes the context of the specified window current on the calling thread. 
 	glfwMakeContextCurrent(m_window);
 
-	//Sets the key callback
-	//glfwSetKeyCallback(m_window, key_callback);
 	glfwSetWindowSizeCallback(m_window, resize_callback);
 
 	glewExperimental = true; // Needed for core profile
@@ -100,12 +83,9 @@ void App::Init(){
 	GLenum err = glewInit();
 
 	//If GLEW hasn't initialized
-	if (err != GLEW_OK) 
-	{
+	if (err != GLEW_OK) {
 		fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
-		//return -1;
 	}
-
 
 	glfwSwapInterval(0);
 }
@@ -120,12 +100,15 @@ void App::Run()
 	while (!glfwWindowShouldClose(m_window) && !IsKeyPressed(VK_ESCAPE))
 	{
 		scene->Update(m_timer.getElapsedTime());
+
+		glViewport(0, 0, winWidth, winHeight);
 		scene->Render();
+
 		//Swap buffers
 		glfwSwapBuffers(m_window);
 		//Get and organize events, like keyboard and mouse input, window resizing, etc...
 		glfwPollEvents();
-        //m_timer.waitUntil(frameTime);       // Frame rate limiter. Limits each frame to a specified time in ms.   
+        //m_timer.waitUntil(frameTime);
 
 	} //Check if the ESC key had been pressed or if the window had been closed
 	scene->Exit();
--- AI/Source/EntryPt.cpp
+++ AI/Source/EntryPt.cpp
@@ -2,6 +2,23 @@
 
 #include <crtdbg.h>
 #include <thread>
+#include <string>
+
+#ifndef SLN_CONFIG
+	#ifdef _DEBUG
+		#define SLN_CONFIG (std::string)"Debug"
+	#else
+		#define SLN_CONFIG (std::string)"Release"
+	#endif
+#endif
+
+#ifndef SLN_PLAT
+	#ifdef _WIN64
+		#define SLN_PLAT (std::string)"x64"
+	#else
+		#define SLN_PLAT (std::string)"x86"
+	#endif
+#endif
 
 bool endLoop;
 
@@ -14,6 +31,9 @@ void MainProcess(){
 
 int main(const int&, const char* const* const&){
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	ShowWindow(GetConsoleWindow(), SW_MAXIMIZE);
+	SetLayeredWindowAttributes(GetConsoleWindow(), NULL, 230, LWA_ALPHA);
+	SetConsoleTitleA(((std::string)"Console Window (" + SLN_CONFIG + ' ' + SLN_PLAT + ')').c_str());
 
 	std::thread worker(&MainProcess);
 
