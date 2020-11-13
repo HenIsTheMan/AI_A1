c10e61a2c098c8e5d046b3cf293a4e38d7f2eb0a
Henry <193541T@mymail.nyp.edu.sg>
2020-11-13 14:12:35 +0800

    Added to StateDead

--- AI/Source/StateDead.h
+++ AI/Source/StateDead.h
@@ -1,1 +1,16 @@
 #pragma once
+
+class StateDead final{ //Static class
+	StateDead() = delete;
+	~StateDead() = delete;
+	StateDead(const StateDead&) = delete;
+	StateDead(StateDead&&) noexcept = delete;
+	StateDead& operator=(const StateDead&) = delete;
+	StateDead& operator=(StateDead&&) noexcept = delete;
+public:
+	static void Enter();
+	static void Update(const double dt);
+	static void Exit();
+};
+
+#include "StateDead.inl"
---
+++ AI/Source/StateDead.inl
@@ -0,0 +1,8 @@
+void StateDead::Enter(){
+}
+
+void StateDead::Update(const double dt){
+}
+
+void StateDead::Exit(){
+}
